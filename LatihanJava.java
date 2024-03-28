883fabe650d1aae79599ccc3afb25a4efe1412a4
RADAFA8642387436 <164320139+RADAFA8642387436@users.noreply.github.com>
2024-03-28 16:27:03 +0700

    Add files via upload

---
+++ LatihanJava.java
@@ -0,0 +1,22 @@
+package latihan.java;
+
+
+
+
+public class LatihanJava {
+
+
+    public static void main(String[] args)  {
+  // TODO code application logic here
+  
+   int jarijari;
+   double keliling;
+   final double PI=3.14;
+
+   jarijari = 30;
+   
+   keliling= 2*PI*jarijari;
+   System.out.println("keliling lingkaran = "+keliling);
+  System.out.println("=========================");
+    }
+}
