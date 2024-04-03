f805edd6909c792d664cac9e699c020f532ec601
RADAFA8642387436 <164320139+RADAFA8642387436@users.noreply.github.com>
2024-04-03 15:24:04 +0700

    Update IfClassAksi.java

--- Latihan3/IfClassAksi.java
+++ Latihan3/IfClassAksi.java
@@ -8,5 +8,17 @@
  * @author radaf
  */
 public class IfClassAksi {
+        public static void main(String[] args) 
+    {
+    double tot_beli;
+    Scanner input = new Scanner(System.in);
+    IfClass fungsiif=new IfClass();
+        
+    System.out.print("Total Pembelian Rp. ");
+    fungsiif.Totalbeli = input.nextDouble();
+        
+    System.out.println("Besarnya Potongan Rp. " + fungsiif.getPotongan());
+    System.out.println("Jumlah yg dibayarkan Rp. " + fungsiif.Jumlahbayar());
+    }
     
 }
